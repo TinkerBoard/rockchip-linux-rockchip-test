477462581f078ce2087c8ec82301b64ebe03bc6f
Caesar Wang <wxt@rock-chips.com>
2022-07-01 16:19:15 +0800

    camera: update rkaiq/rkisp/usb tests
    Signed-off-by: Caesar Wang <wxt@rock-chips.com>
    Change-Id: I730a0f510511398e0392d0bd98028746a3b7f16f

--- camera/camera_usb_test.sh
+++ camera/camera_usb_test.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 #export GST_DEBUG=*:5
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/gstreamer-1.0
-gst-launch-1.0 v4l2src device=/dev/video17 ! image/jpeg, width=1280, height=720, framerate=30/1 ! jpegparse ! mppjpegdec ! waylandsink sync=false
+
+# grep '' /sys/class/video4linux/video*/name
+COMPATIBLE=$(cat /proc/device-tree/compatible)
+if [[ $COMPATIBLE =~ "rk3588" ]]; then
+	gst-launch-1.0 v4l2src device=/dev/video20 ! image/jpeg, width=1280, height=720, framerate=30/1 ! jpegparse ! mppjpegdec ! waylandsink sync=false
+else
+	gst-launch-1.0 v4l2src device=/dev/video10 ! image/jpeg, width=1280, height=720, framerate=30/1 ! jpegparse ! mppjpegdec ! waylandsink sync=false
+fi
