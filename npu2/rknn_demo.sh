ddff484395cec1718c05965348fcca79cc3618b2
Caesar Wang <wxt@rock-chips.com>
2022-08-15 10:55:37 +0800

    npu2: update rknpu2 for rockchip_test
    Signed-off-by: Caesar Wang <wxt@rock-chips.com>
    Change-Id: I11e6269009b5c20ccb19c299f8c3049ff02aa7bd

--- npu2/rknn_demo.sh
+++ npu2/rknn_demo.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-/rockchip-test/npu2/aarch64/rknn_common_test /rockchip-test/npu2/model/RK3588/vgg16_max_pool_fp16.rknn /rockchip-test/npu2/model/dog_224x224.jpg 10
-
+COMPATIBLE=$(cat /proc/device-tree/compatible)
+if [[ $COMPATIBLE =~ "rk3588" ]]; then
+rknn_common_test /usr/share/model/RK3588/mobilenet_v1.rknn /usr/share/model/dog_224x224.jpg 10
+elif [[ $COMPATIBLE =~ "rk3566" && $COMPATIBLE =~ "rk3568" ]]; then
+rknn_common_test /user/share/model/RK356X/mobilenet_v1.rknn /usr/share/model/dog_224x224.jpg 10
+else
+echo "The RKNPU2 did't support this Socs yet..."
+fi
