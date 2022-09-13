df236dc1183f0a85f5aecbb7fc3bfe96516fdb50
Caesar Wang <wxt@rock-chips.com>
2022-09-13 17:18:55 +0800

    rockchip-test: update scripts
    Signed-off-by: Caesar Wang <wxt@rock-chips.com>
    Change-Id: I7c2f42c7d00707d044165fec9547b5c15089edb7

--- npu2/rknn_demo.sh
+++ npu2/rknn_demo.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 COMPATIBLE=$(cat /proc/device-tree/compatible)
-if [[ $COMPATIBLE =~ "rk3588" ]]; then
-rknn_common_test /usr/share/model/RK3588/mobilenet_v1.rknn /usr/share/model/dog_224x224.jpg 10
-elif [[ $COMPATIBLE =~ "rk3566" && $COMPATIBLE =~ "rk3568" ]]; then
-rknn_common_test /user/share/model/RK356X/mobilenet_v1.rknn /usr/share/model/dog_224x224.jpg 10
+if [[ $(expr $COMPATIBLE : ".*rk3588") -ne 0 ]]; then
+	rknn_common_test /usr/share/model/RK3588/mobilenet_v1.rknn /usr/share/model/dog_224x224.jpg 10
+elif [[ $(expr $COMPATIBLE : ".*rk3568") -ne 0 ]]; then
+	rknn_common_test /usr/share/model/RK356X/mobilenet_v1.rknn /usr/share/model/dog_224x224.jpg 10
+elif [[ $(expr $COMPATIBLE : ".*rk3566") -ne 0 ]]; then
+	rknn_common_test /usr/share/model/RK356X/mobilenet_v1.rknn /usr/share/model/dog_224x224.jpg 10
 else
-echo "The RKNPU2 did't support this Socs yet..."
+	echo "The RKNPU2 did't support this Socs yet..."
 fi
